222370ef88ae81bd84ce15e7c9252fc40ba9e4ff
Eduard-02 <eduardocebola83@gmail.com>
2025-05-31 23:04:06 +0100

    Removed the chance of user input being null.

--- Games/GameMenu.cs
+++ Games/GameMenu.cs
@@ -2,10 +2,14 @@ namespace TerminalGameConsole.Games;
 
 public class GameMenu
 {
-    public static void Menu(ref int a)
-    {
-        Console.WriteLine(
-            "Welcome to the Game Menu! Choose one of the games to play:\n\n1 - Play Tic Tac Toe\n2 - Play Connect 4\n0 - Exit\n");
-        a = int.Parse(Console.ReadLine());
-    }
+	public static void Menu(ref int a)
+	{
+		Console.WriteLine("Welcome to the Game Menu! Choose one of the games to play:\n\n1 - Play Tic Tac Toe\n2 - Play Connect 4\n0 - Exit\n");
+		int input;
+		while (!int.TryParse(Console.ReadLine(), out input) || input < 0 || input > 2)
+		{
+			Console.WriteLine($"Invalid input, try again!");
+		}
+		a = input;
+	}
 }
--- Program.cs
+++ Program.cs
@@ -7,43 +7,43 @@ using TerminalGameConsole.Games;
 
 namespace TerminalGameConsole
 {
-    class Program
-    {
-        static int choice = 0;
+	class Program
+	{
+		static int choice = 0;
 
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                GameMenu.Menu(ref choice);
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				GameMenu.Menu(ref choice);
 
-                switch (choice)
-                {
-                    case 0:
-                        choice = 0;
-                        Console.WriteLine("See you soon!");
-                        return;
-                    case 1:
-                        choice = 1;
-                        TicTacToe.Play();
-                        break;
-                    case 2:
-                        choice = 2;
-                        Connect4.InitializeGridConnect();
-                        Connect4.Play();
-                        break;
-                    default:
-                        Console.WriteLine("Invalid option!");
-                        break;
-                }
-                Console.WriteLine("\nPlay another game? (y/n)");
-                string again = Console.ReadLine();
-                if (again != "y")
-                {
-                    Console.WriteLine("\nThank you for playing!");
-                    break;
-                }    
-            }
-        }
-    }
+				switch (choice)
+				{
+					case 0:
+						choice = 0;
+						Console.WriteLine("See you soon!");
+						return;
+					case 1:
+						choice = 1;
+						TicTacToe.Play();
+						break;
+					case 2:
+						choice = 2;
+						Connect4.InitializeGridConnect();
+						Connect4.Play();
+						break;
+					default:
+						Console.WriteLine("Invalid option!");
+						break;
+				}
+				Console.WriteLine("\nPlay another game? (y/n)");
+				string? again = Console.ReadLine();
+				if (again != "y")
+				{
+					Console.WriteLine("\nThank you for playing!");
+					break;
+				} 
+			}
+		}
+	}
 }    
